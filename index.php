ca6eb4b533181853d4b9b0f829973e87eafd668d
johnalexf <johnalexfr@gmail.com>
2026-06-22 10:37:54 -0500

    style(index.php): agregar tipografías de Google Fonts

--- index.php
+++ index.php
@@ -7,6 +7,18 @@
 
     <!-- Bootstrap CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    
+    <!-- Google Fonts -->
+        <!-- Tipografías: Montserrat para títulos y Roboto para textos generales -->
+
+        <!-- Optimiza la conexión con los servidores de Google Fonts antes de cargar las fuentes -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <!-- Carga las familias tipográficas Montserrat y Roboto -->
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <!-- Fin Google Fonts -->
+     
 </head>
 <body>
 
