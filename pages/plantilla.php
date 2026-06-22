ffff077aa4c74af9bfd57e724bf26e2eb7f75f91
johnalexf <johnalexfr@gmail.com>
2026-06-22 11:37:00 -0500

    feat(pages): crear plantilla base con rutas corregidas para nuevas paginas

---
+++ pages/plantilla.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plantilla - JAFR</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    
+    <!-- Google Fonts -->
+        <!-- Tipografías: Montserrat para títulos y Roboto para textos generales -->
+
+        <!-- Optimiza la conexión con los servidores de Google Fonts antes de cargar las fuentes -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <!-- Carga las familias tipográficas Montserrat y Roboto -->
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <!-- Fin Google Fonts -->
+
+    <!-- Estilos propios -->
+    <link href="../assets/css/global.css" rel="stylesheet">
+
+</head>
+<body>
+
+    <div class="container my-5 text-center">
+        <h1>Nueva Página</h1>
+        <p>Estructura base lista.</p>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+    <!-- Iconify -->
+    <script src="../vendor/iconify/js/iconify-icon.min.js"></script>
+</body>
+</html>
