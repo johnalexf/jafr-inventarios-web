782931ff31008c1c42d697ea45fe8437d30188fc
johnalexf <johnalexfr@gmail.com>
2026-06-26 15:52:01 -0500

    feat(usuarios): Crear pagina con bosquejo desde plantilla.php

---
+++ pages/usuarios.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Icono para pestañas en navegadores web-->
+    <link rel="icon" type="image/x-icon" href="../assets/img/favicon.ico">
+
+    <title>Usuarios - JAFR</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    
+    <!-- Google Fonts -->
+        <!-- Tipografías: Montserrat para títulos y Roboto para textos generales -->
+
+        <!-- Optimiza la conexión con los servidores de Google Fonts antes de cargar las fuentes -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <!-- Carga las familias tipográficas Montserrat y Roboto -->
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <!-- Fin Google Fonts -->
+
+    <!-- Estilos propios -->
+    <link href="../assets/css/global.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/formularios.css">
+
+</head>
+<body class="vh-100 d-flex flex-column">
+
+    <!-- Barra superior con logo, título y menú desplegable con offcanvas -->
+    <header class="p-1">
+
+        <!-- Contenedor principal con diseño navbar aun que no es directamente el nav tiene el boton que lleva al navbar  -->
+        <div class="navbar bg-white rounded-1 shadow-sm">
+
+            <!-- Contenedor secundario por defecto configuracion de boostrap -->
+            <div class="container-fluid d-flex justify-content-between align-items-center">
+                
+                <!-- Logotipo de la pagina (Enlace al home) -->
+                <a class="navbar-brand m-0 p-0" href="inicio.php">
+                    <img src="../assets/img/logo.png" alt="Logo" class="logo">
+                </a>
+                
+                <!-- Titulo con nombre de la empresa y seccion en donde se encuentra el usuario (el nombre de la empresa se oculta en pantallas pequeñas para mejorar la visualizacion)-->
+                <h1 class="d-flex align-items-center text-primary pe-4 pe-md-0 mb-0 fw-bold fs-3">
+                    <span class=" d-none d-lg-block ">
+                        Nombre de la empresa /
+                    </span>
+                    <iconify-icon 
+                        icon="mdi:users" 
+                        class="px-1 fs-2" >
+                    </iconify-icon>
+                    <span>Usuarios</span>    
+                </h1>
+
+                <!-- Contenedor del rol y del boton de menu de navegacion -->
+                <div class="d-flex gap-3">
+                    
+                    <!-- Contenedor del rol del usuario -->
+                    <div class="px-3 border rounded-4 bg-surface d-none d-md-flex align-items-center">
+                        <p class="mb-0 fw-semibold fs-6">
+                            Administrador
+                        </p>
+                    </div>
+                    
+                    <!-- boton del menu -->
+                    <button 
+                        class="btn btn-primary rounded-4 d-flex align-items-center gap-2"
+                        type="button"
+                        data-bs-toggle="offcanvas" 
+                        data-bs-target="#menuLateral" 
+                        aria-controls="menuLateral"
+                    >
+
+                        <span class="mb-0 fw-semibold fs-6 d-none d-md-block" >
+                            Menú
+                        </span>
+                        <!-- Icono de menu lateral para pantallas menores a lg -->
+                        <!-- Por medio de offcanvas de boostrap podemos mostrar un menu lateral -->
+                        <iconify-icon 
+                            icon="mdi:menu" 
+                            class="fs-2"
+                            >
+                        </iconify-icon>
+
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </header>
+
+    <main class="flex-grow-1 overflow-y-auto p-3 p-md-4">
+
+        
+
+        <!-- boton para activar el modal de ejemplo -->
+        <button 
+            type="button" 
+            class="btn btn-link p-0 link-primary fw-bold" 
+            data-bs-toggle="modal" 
+            data-bs-target="#modalEditarUsuario"
+        >
+            Activar modal
+        </button>
+        
+
+    </main>
+    
+
+    <footer class="p-1">
+        <div class="container-fluid bg-white border-top py-2 text-center text-muted rounded-1">
+            <small>&copy; 2026 JAFR - Administra tu negocio. <span class="d-block d-sm-inline">Prototipo de interfaz.</span></small>
+        </div>
+    </footer>
+
+
+    <!-- Contenedor principal del menu lateral se activa con el boton en el header-->
+    <nav class="offcanvas offcanvas-end rounded-4" tabindex="-1" id="menuLateral" aria-labelledby="etiquetaMenuLateral" style = "max-height: 620px; width:270px">
+    
+        <!-- Encabezado del menu lateral -->
+        <div class="offcanvas-header border-bottom">
+
+            <!-- Identificacion del rol en el menú -->
+            <div class="d-flex align-items-center text-primary">
+                <iconify-icon icon="mdi:account-badge" class="fs-1 me-2"></iconify-icon>
+                <h3 class="offcanvas-title fs-5 fw-bold" id="etiquetaMenuLateral">Menu  Administrador</h3>
+            </div>
+
+            <!-- Boton para cerrar el menu lateral -->
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        
+        <!-- Cuerpo del menu lateral (lista de enlaces a los modulos) -->
+        <div class="offcanvas-body overflow-y-auto">
+            <div class="d-flex flex-column gap-4 px-3 mt-3">
+                
+                <a href="inicio.php" class="d-flex align-items-center link-primary fw-bold fs-5">
+                    <iconify-icon icon="mdi:home" class="fs-3 me-3"></iconify-icon> Inicio
+                </a>
+                
+                <a href="usuarios.php" class="d-flex align-items-center link-secondary fw-bold fs-5">
+                    <iconify-icon icon="mdi:users" class="fs-3 me-3"></iconify-icon> Usuarios
+                </a>
+                
+                <a href="productos.php" class="d-flex align-items-center link-primary fw-bold fs-5">
+                    <iconify-icon icon="mdi:bread" class="fs-3 me-3"></iconify-icon> Productos
+                </a>
+                
+                <a href="proveedores.php" class="d-flex align-items-center link-primary fw-bold fs-5">
+                    <iconify-icon icon="clarity:building-line" class="fs-3 me-3"></iconify-icon> Proveedores
+                </a>
+                
+                <a href="clientes.php" class="d-flex align-items-center link-primary fw-bold fs-5">
+                    <iconify-icon icon="bxs:store" class="fs-3 me-3"></iconify-icon> Clientes
+                </a>
+                
+                <a href="compras.php" class="d-flex align-items-center link-primary fw-bold fs-5">
+                    <iconify-icon icon="fa7-solid:bag-shopping" class="fs-3 me-3"></iconify-icon> Compras
+                </a>
+                
+                <a href="ventas.php" class="d-flex align-items-center link-primary fw-bold fs-5">
+                    <iconify-icon icon="bi:cash-coin" class="fs-3 me-3"></iconify-icon> Ventas
+                </a>
+                
+                <a href="inventario.php" class="d-flex align-items-center link-primary fw-bold fs-5">
+                    <iconify-icon icon="mingcute:cube-fill" class="fs-3 me-3"></iconify-icon> Inventario
+                </a>
+                
+                <a href="reporte.php" class="d-flex align-items-center link-primary fw-bold fs-5">
+                    <iconify-icon icon="mdi:file-chart" class="fs-3 me-3"></iconify-icon> Reporte
+                </a>
+
+            </div>
+        </div>
+    </nav>
+
+     <!-- Modal de Ejemplo con formulario -->
+    <div class="modal fade" id="modalEditarUsuario" tabindex="-1" 
+        aria-labelledby="modalTitulo-EditarUsuario" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+
+            <!-- Contenido del modal que va mostrar un formulario -->
+            <form class="modal-content" id="formularioEditarUsuario">
+
+                <!-- Titulo del modal y boton cerrar modal -->
+                <div class="modal-header position-relative">
+                    <h3 class="modal-title fs-5 fw-bold mx-auto" id="modalTitulo-EditarUsuario">
+                        Editar Usuario
+                    </h3>
+                    <button 
+                        type="button" 
+                        class="btn btn-link link-dark d-flex position-absolute end-0 top-0 p-2 opacity-75" 
+                        data-bs-dismiss="modal" 
+                        aria-label="Close">
+                            <iconify-icon
+                                icon="carbon:close-filled"
+                                class="my-auto fs-3">
+                            </iconify-icon>
+                    </button>
+                </div>
+
+                <!-- Cuerpo del modal contenido de los input del formulario-->
+                <div class="modal-body px-4">
+                
+                    <!-- Seccion del formulario para editar nombre completo -->
+                    <fieldset class="mb-3 px-2">
+                        <legend class="fs-6 fw-bold border-bottom border-dark mb-3 ps-0 ps-sm-4">
+                            Cuenta
+                        </legend>
+
+                        <div class="jafr-form-group">
+                            <label for="alias" class="jafr-form-label">Alias:</label>
+                            <div class="jafr-form-input-container">
+                                <input type="text" id="alias" name="alias" class="form-control form-control-sm border border-black">                                        
+                            </div>
+                        </div>
+                    </fieldset>
+
+                    <!-- Seccion del formulario para editar nombre completo -->
+                    <fieldset class="mb-3 px-2">
+                        <legend class="fs-6 fw-bold border-bottom border-dark mb-3 ps-0 ps-sm-4">
+                            Nombre Completo
+                        </legend>
+
+                        <div class="jafr-form-group">
+                            <label for="primerNombre" class="jafr-form-label">Primer Nombre:</label>
+                            <div class="jafr-form-input-container">
+                                <input type="text" id="primerNombre" name="primerNombre" class="form-control form-control-sm border border-black">                                        
+                            </div>
+                        </div>
+                        
+                        <div class="jafr-form-group">
+                            <label for="segundoNombre" class="jafr-form-label">Segundo Nombre:</label>
+                            <div class="jafr-form-input-container">
+                                <input type="text" id="segundoNombre" name="segundoNombre" class="form-control form-control-sm border border-black">                                        
+                            </div>
+                        </div>
+
+                        <div class="jafr-form-group">
+                            <label for="primerApellido" class="jafr-form-label">Primer apellido:</label>
+                            <div class="jafr-form-input-container">
+                                <input type="text" id="primerApellido" name="primerApellido" class="form-control form-control-sm border border-black">                                        
+                            </div>
+                        </div>
+
+                        <div class="jafr-form-group">
+                            <label for="segundoApellido" class="jafr-form-label">Segundo apellido:</label>
+                            <div class="jafr-form-input-container">
+                                <input type="text" id="segundoApellido" name="segundoApellido" class="form-control form-control-sm border border-black">                                        
+                            </div>
+                        </div>
+                    </fieldset>
+                    <!-- Fin Seccion del formulario para editar nombre completo -->
+
+
+                    <!-- Seccion del formulario para editar datos de contacto -->
+                    <fieldset class="px-2">
+                        <legend class="fs-6 fw-bold border-bottom border-dark mb-3 ps-0 ps-sm-4">
+                            Datos de contacto
+                        </legend>
+
+                        <div class="jafr-form-group">
+                            <label for="telefono" class="jafr-form-label">Teléfono:</label>
+                            <div class="jafr-form-input-container">
+                                <input type="tel" id="telefono" name="telefono" class="form-control form-control-sm border border-black">                                        
+                            </div>
+                        </div>
+
+                        <div class="jafr-form-group">
+                            <label for="correo" class="jafr-form-label">Correo:</label>
+                            <div class="jafr-form-input-container">
+                                <input type="email" id="correo" name="correo" class="form-control form-control-sm border border-black">                                        
+                            </div>
+                        </div>
+                    </fieldset>
+                    <!-- Fin Seccion del formulario para editar datos de contacto -->
+
+                </div>
+
+                <!-- Footer del modal contiene los botones de acción -->
+                <div class="modal-footer flex-column justify-content-center">
+                    <button 
+                        type="submit" 
+                        class="btn btn-primary fw-bold px-5 shadow" 
+                        data-bs-dismiss="modal">
+                            Actualizar
+                    </button>
+                    <!-- Contenedor acciones de editar otra informacion o (deshabilitar o eliminar)modificar el estado de un objeto -->
+                    <div class="w-100 d-flex justify-content-start"> 
+                        <button
+                            type="button"
+                            class="btn btn-link link-secondary py-0 fw-bold"
+                        >
+                            Editar contraseña
+                        </button>
+                    </div>
+                </div>
+
+            </form>
+
+        </div>
+    </div>
+
+
+    <!-- Bootstrap JS -->
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+    <!-- Iconify -->
+    <script src="../vendor/iconify/js/iconify-icon.min.js"></script>
+</body>
+</html>
